b0db36f7613e938d77193597aefaf9d18048d5cf
s632061 <s632061@gmail.com>
2026-09-17 17:04:09 -0400

    Latest WMOL site updates for staging

--- WMOL/urls.py
+++ WMOL/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("tools/aws-saa/", TemplateView.as_view(template_name="landing/aws-saa.html"), name="aws_saa_companion"),
     path("", include(("LandingPage.urls", "landing"), namespace="landing")),
 ]
 
